48c0ed6effff432221ffd5ea019a54f0f576408f
Arexh <644564286@qq.com>
2022-05-30 16:22:57 +0800

    finish final starrc

---
+++ final/starrc/starrc.cmd
@@ -0,0 +1,21 @@
+
+BLOCK: RISC8_routed
+MILKYWAY_DATABASE: ../icc/risc8.mw 
+MAPPING_FILE: ./saed32nm_tf_itf_tluplus.map
+REDUCTION: NO_EXTRA_LOOPS
+
+EXTRACTION: RC
+
+NETLIST_FORMAT: SPEF
+NETLIST_FILE: ./max.spef
+
+SUMMARY_FILE:  RISC_CHIP.summary
+STAR_DIRECTORY:  ./starrc_dir
+
+BUS_BIT: []
+HIERARCHICAL_SEPARATOR: /
+
+
+OPERATING_TEMPERATURE: 25
+TCAD_GRD_FILE: ./saed32nm_1p9m_nominal.nxtgrd
+                                                                                            
